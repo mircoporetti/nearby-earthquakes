abd246b15a5feebfc7fb6e226f621f729b4da6cb
Mirco Poretti <mircoporetti@gmail.com>
2021-02-20 11:14:53 +0100

    No Earthquakes in the last 30 days

--- src/main/java/me/mircoporetti/nearbyearthquakes/domain/earthquake/usecase/NearbyEarthquakes.java
+++ src/main/java/me/mircoporetti/nearbyearthquakes/domain/earthquake/usecase/NearbyEarthquakes.java
@@ -1,12 +1,27 @@
 package me.mircoporetti.nearbyearthquakes.domain.earthquake.usecase;
 
+import me.mircoporetti.nearbyearthquakes.domain.earthquake.entity.Earthquake;
+import me.mircoporetti.nearbyearthquakes.domain.earthquake.port.USGSEarthquakePort;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class NearbyEarthquakes implements NearbyEarthquakesUseCase{
+    private final USGSEarthquakePort usgsEarthquakePort;
+
+    public NearbyEarthquakes(USGSEarthquakePort usgsEarthquakePort) {
+        this.usgsEarthquakePort = usgsEarthquakePort;
+    }
+
     @Override
     public List<EarthquakeResponseModel> execute(NearbyEarthquakesCoordinateRequestModel coordinateRequestModel) {
         if(coordinateRequestModel.isAValidEarthCoordinate()){
-            return null;
+            List<Earthquake> lastThirtyDaysEarthquakes = usgsEarthquakePort.getLastThirtyDaysEarthquakes();
+
+            return Collections.emptyList();
         }else{
             throw new NotAnEarthCoordinateException(
                     String.format("Given lat: %s lon: %s is not a valid earth coordinate",
--- src/test/java/me/mircoporetti/nearbyearthquakes/domain/earthquake/usecase/NearbyEarthquakesUseCaseTest.java
+++ src/test/java/me/mircoporetti/nearbyearthquakes/domain/earthquake/usecase/NearbyEarthquakesUseCaseTest.java
@@ -1,11 +1,20 @@
 package me.mircoporetti.nearbyearthquakes.domain.earthquake.usecase;
 
 import me.mircoporetti.nearbyearthquakes.domain.earthquake.port.USGSEarthquakePort;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 class NearbyEarthquakesUseCaseTest {
@@ -17,7 +26,7 @@ class NearbyEarthquakesUseCaseTest {
     @BeforeEach
     void setUp() {
         initMocks(this);
-        underTest = new NearbyEarthquakes();
+        underTest = new NearbyEarthquakes(usgsEarthquakePort);
     }
 
     @Test
@@ -25,6 +34,18 @@ class NearbyEarthquakesUseCaseTest {
         NearbyEarthquakesCoordinateRequestModel givenCoordinate = new NearbyEarthquakesCoordinateRequestModel(-91, 181);
 
         assertThrows(NotAnEarthCoordinateException.class, () -> underTest.execute(givenCoordinate));
+    }
+
+    @Test
+    void noEarthquakesFound() {
+        doReturn(emptyList()).when(usgsEarthquakePort).getLastThirtyDaysEarthquakes();
+
+        NearbyEarthquakesCoordinateRequestModel givenCoordinate = new NearbyEarthquakesCoordinateRequestModel(0.0, 0.0);
+
+        List<EarthquakeResponseModel> result = underTest.execute(givenCoordinate);
+
+        verify(usgsEarthquakePort).getLastThirtyDaysEarthquakes();
+        assertThat(result, Matchers.is(emptyList()));
     }
 
 }
